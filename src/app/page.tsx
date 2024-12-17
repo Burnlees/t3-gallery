d09d803392c2d90ec4b2c11c50afeb99d1096180
burnlees <josh.burnlees@outlook.com>
2024-12-17 22:33:18 +0000

    feat: scaffold ui with mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
+const mockUrls = [
+  "https://utfs.io/f/3en6V5DwL4xhnSWvFtxaW3XrgP9mfOz6oJv5NT4GSc2CV8KQ",
+  "https://utfs.io/f/3en6V5DwL4xhPQTFvcSTEyvBLKxQR7Xi9fah6rHsCYZDSj8J",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[
+          ...mockImages,
+          ...mockImages,
+          ...mockImages,
+          ...mockImages,
+          ...mockImages,
+          ...mockImages,
+          ...mockImages,
+          ...mockImages,
+          ...mockImages,
+        ].map((image) => {
+          return (
+            <div key={image.id} className="w-48 p-4">
+              <img src={image.url} />
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 }
