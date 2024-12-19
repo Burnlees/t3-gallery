7aaf1696df008554fc5a2f24d0e922f157159e75
burnlees <josh.burnlees@outlook.com>
2024-12-19 17:44:33 +0000

    feat: fixed dialog full screen, created component for image view

--- src/app/photos/[id]/page.tsx
+++ src/app/photos/[id]/page.tsx
@@ -1,8 +1,14 @@
-export default async function PhotoModal({
-    params,
-  }: {
-    params: Promise<{ id: string }>;
-  }) {
-    const photoId = (await params).id;
-    return <div>{photoId}</div>;
-  }
+import { getImage } from "~/server/queries";
+import FullPageImageView from "~/components/full-image-page";
+
+export default async function PhotoPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const idAsNumber = Number((await params).id);
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid id");
+
+  const image = await getImage(idAsNumber);
+  return <FullPageImageView id={idAsNumber} />;
+}
