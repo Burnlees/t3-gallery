6c2cb66e5f2f3ad1f244c6eb8416403ddb73198b
burnlees <josh.burnlees@outlook.com>
2024-12-19 11:55:39 +0000

    feat: uploadthing added

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -31,13 +31,18 @@ export const ourFileRouter = {
       return { userId: user.userId };
     })
     .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
+
+      console.log("file url", file.url);
 
       await db.insert(images).values({
         name: file.name,
         url: file.url,
+        userId: metadata.userId,
       });
 
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId };
     }),
 } satisfies FileRouter;
